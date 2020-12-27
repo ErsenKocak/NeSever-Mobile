27fa8f352c6055fba7cd37705c07beceec9696e7
ErsenKocak <ersenkocak9@gmail.com>
2020-12-27 16:11:59 +0300

    -CategoryListView and ExpansionTileList bugs fixed and tested.

--- lib/views/category/view/category_view.dart
+++ lib/views/category/view/category_view.dart
@@ -21,6 +21,7 @@ class _CategoryViewState extends State<CategoryView> {
   bool isActive = false;
   CategoryModel selectedCategory;
   CategoryModel selectedSubCategory;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,10 +58,7 @@ class _CategoryViewState extends State<CategoryView> {
                   } else if (state is CategoryLoadedState) {
                     return Row(
                       children: [
-                        Expanded(
-                          flex: 2,
-                          child: buildCategoryListView(state),
-                        ),
+                        Expanded(flex: 2, child: buildCategoryListView(state)),
                       ],
                     );
                   }
@@ -78,7 +76,10 @@ class _CategoryViewState extends State<CategoryView> {
       children: [
         Expanded(
           flex: 2,
-          child: ListView.builder(
+          child:
+              //buildCategoryList(state),
+
+              ListView.builder(
             itemCount: state.categoryList.length,
             itemBuilder: (BuildContext context, int index) {
               if (state.categoryList[index].ustKategoriId == null) {
@@ -100,102 +101,120 @@ class _CategoryViewState extends State<CategoryView> {
             },
           ),
         ),
-
         Expanded(
           flex: 8,
-          //TODO: IS Working
           child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.categoryList.length,
-                  itemBuilder: (context, index) {
-                    if (state.categoryList[index].ustKategoriId != null &&
-                        selectedCategory != null &&
-                        state.categoryList[index].ustKategoriId ==
-                            selectedCategory.kategoriId) {
-                      return ExpansionTileCard(
-                        initialElevation: 4,
-                        shadowColor: kSecondaryColor,
-                        baseColor: kLightPrimaryColor,
-                        expandedColor: kLightPrimaryColor,
-                        onExpansionChanged: (value) {
-                          print(state.categoryList[index].kategoriAdi);
-                          setState(() {
-                            selectedSubCategory = state.categoryList[index];
-                          });
-                        },
-                        title: Text(
-                          state.categoryList[index].kategoriAdi,
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        children: <Widget>[
-                          Divider(
-                            thickness: 1.0,
-                            height: 1.0,
-                          ),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                                vertical: 8.0,
-                              ),
-                              child: ListView.builder(
-                                itemCount: state.categoryList.length,
-                                itemBuilder: (context, index) {
-                                  if (selectedSubCategory != null &&
-                                      state.categoryList[index].ustKategoriId ==
-                                          selectedSubCategory.kategoriId) {
-                                    return Text(
-                                        state.categoryList[index].kategoriAdi);
-                                  } else {
-                                    return SizedBox();
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                          ButtonBar(
-                            alignment: MainAxisAlignment.spaceAround,
-                            buttonHeight: 52.0,
-                            buttonMinWidth: 90.0,
-                            children: <Widget>[],
-                          ),
-                        ],
-                      );
-                    } else {
-                      return SizedBox();
-                    }
-                  },
-                ),
-              )
-            ],
+            children: [Expanded(child: buildExpansionTileList(state))],
           ),
         ),
-
-        // Expanded(
-        //   //TODO: IS Working
-        //   child: Column(
-        //     children: [
-        //       Expanded(
-        //         child: ListView.builder(
-        //           itemCount: state.categoryList.length,
-        //           itemBuilder: (context, index) {
-        //             if (selectedCategory != null &&
-        //                 state.categoryList[index].ustKategoriId ==
-        //                     selectedCategory.kategoriId) {
-        //               return Text(state.categoryList[index].kategoriAdi);
-        //             } else {
-        //               return SizedBox();
-        //             }
-        //           },
-        //         ),
-        //       )
-        //     ],
-        //   ),
-        // ),
       ],
+    );
+  }
+
+  buildExpansionTileList(CategoryLoadedState state) {
+    final expansionTileList = List<Widget>();
+    state.categoryList
+        .map((e) => {
+              if (e.ustKategoriId != null &&
+                  selectedCategory != null &&
+                  e.ustKategoriId == selectedCategory.kategoriId)
+                {
+                  expansionTileList.add(ExpansionTileCard(
+                    initialElevation: 4,
+                    shadowColor: kSecondaryColor,
+                    baseColor: kLightPrimaryColor,
+                    expandedColor: kLightPrimaryColor,
+                    onExpansionChanged: (value) {
+                      print(e.kategoriAdi);
+                      setState(() {
+                        selectedSubCategory = e;
+                      });
+                    },
+                    title: Text(
+                      e.kategoriAdi,
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    children: <Widget>[
+                      Divider(
+                        thickness: 1.0,
+                        height: 1.0,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                            vertical: 8.0,
+                          ),
+                          child: ListView.builder(
+                            itemCount: state.categoryList.length,
+                            itemBuilder: (context, index) {
+                              if (selectedSubCategory != null &&
+                                  state.categoryList[index].ustKategoriId ==
+                                      selectedSubCategory.kategoriId) {
+                                return Text(
+                                    state.categoryList[index].kategoriAdi);
+                              } else {
+                                return SizedBox();
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.spaceAround,
+                        buttonHeight: 52.0,
+                        buttonMinWidth: 90.0,
+                        children: <Widget>[],
+                      ),
+                    ],
+                  ))
+                }
+              else
+                {SizedBox()}
+            })
+        .toList();
+
+    return SingleChildScrollView(
+      child: Column(
+        children: expansionTileList,
+      ),
+    );
+  }
+
+  buildCategoryList(CategoryLoadedState state) {
+    final categoryWidgetList = List<Widget>();
+
+    state.categoryList
+        .map(
+          (category) => {
+            if (category.ustKategoriId == null)
+              {
+                categoryWidgetList.add(
+                  CategoryCard(
+                    key: Key('counter-${category.kategoriId}'),
+                    category: category,
+                    icon: 'assets/icons/book.svg',
+                    text: category.kategoriAdi,
+                    press: () {
+                      setState(() {
+                        selectedCategory = category;
+                      });
+                      print(selectedCategory.kategoriAdi);
+                    },
+                  ),
+                )
+              },
+          },
+        )
+        .toList();
+
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: categoryWidgetList,
+        ),
+      ),
     );
   }
 }
